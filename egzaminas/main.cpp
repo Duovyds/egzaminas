c738190d9e602e5ea4f471782182b63b8aeb8032
Duovyds <k.duovyds@gmail.com>
2024-12-23 18:16:12 +0200

    Initial Commit

---
+++ egzaminas/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  egzaminas
+//
+//  Created by Dovydas Krištupas on 23/12/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
