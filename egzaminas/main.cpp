a26132ecf93d1c4b9c50da3c07744474f950bd3c
Duovyds <k.duovyds@gmail.com>
2024-12-23 20:30:01 +0200

    Sukurta funkcija, kuri sukuria cross-reference lentelę. T.y., vietoje žodžių pasikartojimo skaičiaus faile dabar rašomos eilutės, kuriuose tie žodžiai atrasti.

--- egzaminas/main.cpp
+++ egzaminas/main.cpp
@@ -1,14 +1,8 @@
-//
-//  main.cpp
-//  egzaminas
-//
-//  Created by Dovydas Krištupas on 23/12/2024.
-//
-
 #include <iostream>
 #include "header.h"
 
-
+// Pirma uzduotis
+// ---------------------------------------------------------------------------------------------------------------
 
 unordered_map<string, int> skaitymas_is_failo(){
     
@@ -61,9 +55,9 @@ void tikrinimas(unordered_map<string, int > &hm) {
 
 
 
-void irasymas_i_faila(unordered_map<string, int > &hm){
+void irasymas_i_faila(unordered_map<string, int > &hm, string pav){
     
-    string failo_pav = string("/Users/dovydaskr/Documents/C++/egzaminas/egzaminas/rez.txt");
+    string failo_pav = string("/Users/dovydaskr/Documents/C++/egzaminas/egzaminas/" + pav + ".txt");
     ofstream rez;
     rez.open(failo_pav);
     
@@ -73,6 +67,64 @@ void irasymas_i_faila(unordered_map<string, int > &hm){
     rez.close();
 }
 
+// ---------------------------------------------------------------------------------------------------------------
+
+
+
+// Antra uzduotis
+// ---------------------------------------------------------------------------------------------------------------
+
+unordered_map<string, vector<int>> cross_reference_lentele(unordered_map<string, int > &hm){
+    
+    std::unordered_map<string, vector<int>> hm2;
+    string failo_pav = string("/Users/dovydaskr/Documents/C++/egzaminas/egzaminas/input.txt");
+    ifstream failas;
+    failas.open(failo_pav);
+    string eilute;
+    string zodis;
+    int eilutes_counter = 0;
+    vector<string> zodziu_eilute;
+    
+    if (!failas) {
+        throw runtime_error("Problemos su failo atidarymu");
+    }
+    
+    while (getline(failas, eilute)){
+        stringstream stream(eilute);
+        eilutes_counter++;
+        zodziu_eilute.clear();
+        
+        while(stream >> zodis){
+            zodziu_eilute.push_back(zodis);
+            
+            if (hm2.find(zodis) == hm2.end()) {
+                // If the word is not already in the map, initialize it with the current line
+                hm2[zodis] = {eilutes_counter};
+            } else {
+                // If the word is already in the map, ensure the line number is not duplicated
+                if (hm2[zodis].back() != eilutes_counter) {
+                    hm2[zodis].push_back(eilutes_counter);
+                }
+            }
+        }
+    }
+    failas.close();
+    
+    string failo_pav2 = string("/Users/dovydaskr/Documents/C++/egzaminas/egzaminas/rez2.txt");
+    ofstream rez;
+    rez.open(failo_pav2);
+    
+    for (const auto& pair : hm2) {
+            rez << pair.first << ": ";
+            for (int line : pair.second) {
+                rez << line << " ";
+            }
+            rez << endl;
+        }
+    
+    return hm2;
+};
+
 
 
 int main(int argc, const char * argv[]) {
@@ -80,11 +132,9 @@ int main(int argc, const char * argv[]) {
     
     std::unordered_map<string, int> hm = skaitymas_is_failo();
     tikrinimas(hm);
-    irasymas_i_faila(hm);
+    irasymas_i_faila(hm, "rez1");
+    std::unordered_map<string, vector<int>> hm2 = cross_reference_lentele(hm);
     
-    for (const auto &x : hm) {
-            cout << x.first << ": " << x.second << endl;
-        }
     
     
     
