b72d5a8456ea846bee68e2f8fa5b04d869c8bd0a
Dovydas <88598029+Duovyds@users.noreply.github.com>
2024-12-30 12:10:19 +0200

    Pridedami header ir input failai.

---
+++ egzaminas/header.h
@@ -0,0 +1,31 @@
+#ifndef header_h
+#define header_h
+
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <regex>
+
+using std::string;
+using std::wstring;
+using std::cout;
+using std::endl;
+using std::unordered_map;
+using std::ifstream;
+using std::runtime_error;
+using std::ofstream;
+using std::stringstream;
+using std::vector;
+using std::regex;
+using std::wregex;
+using std::smatch;
+using std::regex_search;
+using std::wsregex_iterator;
+using std::wifstream;
+using std::wstring;
+using std::wofstream;
+
+
+
+#endif /* header_h */
